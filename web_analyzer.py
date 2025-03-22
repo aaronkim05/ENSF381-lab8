3a1772c7abd0a0ce791a917fb8b55c63d51a45f5
cleipoldt <ccpleipoldt@gmail.com>
2025-03-21 20:43:30 -0600

    So far...

--- web_analyzer.py
+++ web_analyzer.py
@@ -1,3 +1,5 @@
+
+import matplotlib.pyplot as plt
 import requests
 from bs4 import BeautifulSoup
 url = "https://en.wikipedia.org/wiki/University_of_Calgary"
@@ -13,6 +15,7 @@ except Exception as e:
 
 print(soup.prettify())
 
+# 3 --------------------------------------
 
 headings = 0
 for i in range(1, 7):
@@ -28,73 +31,36 @@ paragraphs = 0
 paragraphs += len(soup.find_all(f"p"))
 print(f"Number of paragraphs: {paragraphs}")
 
-# count = input("Enter keyword to find count of it in webpage: ")
-# count_user_input = 0
-# count_user_input += len(soup.find_all(count))
+# 4 -------------------------
 
-# print(f"The user input is count is {count_user_input}")
+text = soup.get_text()
+words = text.lower().split()
+key_words = []
 
-extracted_text = soup.get_text().split()
+for i in words:
+    key_word = "".join(char for char in i if char.isalnum())
+    if key_word:
+        key_words.append(key_word)
 
-new_list = []
-for stuff in extracted_text:
-    new_list.append(stuff.lower())
-print(new_list)
+user_key_word = input("Enter keyword: ").strip()
 
-new_list_copy = new_list.copy()
+key_word_count = key_words.count(user_key_word.lower())
 
-brand_new_list = []
-
-for i in new_list_copy:
-    if i not in brand_new_list:
-        brand_new_list.append(i)
+print(f"The keyword {user_key_word} appears {key_word_count} times")
 
 
-print(brand_new_list)
+# 5 -------------------------------
 
+word_frequencies = {}
 
-empty_array = []
-for i in brand_new_list:
-    count = 0 
-    while i in new_list_copy:
-        new_list_copy.remove(i)
-        count += 1
+for i in key_words:
+    word_frequencies[i] = word_frequencies.get(i, 0) + 1
 
-    empty_array.append(count)
-    print(f"Frequency for {i} is: {count}")
+top_five_words = sorted(word_frequencies.items(), key=lambda x: x[1], reverse=True)[:5]
 
-print(empty_array)
+print("Top 5 words with most frequencies:")
+for i, j in top_five_words:
+    print(f"{i}: {j}")
 
-most_frequent_in_order = []
-
-for i in range(5):
-   
-    frequent1 = max(empty_array)
-
-    frequent1_index = empty_array.index(frequent1)
-    most_frequent_in_order.append(brand_new_list[frequent1_index])
-    empty_array.pop(frequent1_index)
-
-print(most_frequent_in_order)
-
-
-# Possible solutions at the bottom 
-
-# for keywords -- I think this is what they meant (but its copied) (maybe we should ask prof what exactly they mean)
-
-cleaned_words = []
-for word in words:
-    clean_word = "".join(char for char in word if char.isalnum())
-    if clean_word:
-        cleaned_words.append(clean_word)
-
-
-
-
-
-
-
-
-
-
+# 6 -------------------------------
 
